100d52c8e2debc70bda5488c4550fdb26854e360
hvidal <hectorvidalsoto@gmail.com>
2026-04-22 10:55:02 +0200

    todo los test realizados

--- src/test/java/org/ed05_1/GestorJuegosTest.java
+++ src/test/java/org/ed05_1/GestorJuegosTest.java
@@ -6,12 +6,37 @@ import org.junit.jupiter.api.Test;
 
 public class GestorJuegosTest {
 
-  GestorJuegos tienda = new GestorJuegos();
+  GestorJuegos gestorJuegos = new GestorJuegos();
+
+  @Test
+  public void cp1() {
+    String[] codigos = {"ABC123"};
+    int[] unidades = {13,4};
+    assertEquals(-1, gestorJuegos.registrarLoteJuegos(codigos, unidades));
+  }
+  @Test
+  public void cp2() {
+    String[] codigos = {};
+    int[] unidades = {7,6,5};
+    assertEquals(-1, gestorJuegos.registrarLoteJuegos(codigos, unidades));
+  }
+  @Test
+  public void cp3() {
+    String[] codigos = {"ABC123", "DEF456","GHI789"};
+    int[] unidades = {-7,6,5};
+    assertEquals(-2, gestorJuegos.registrarLoteJuegos(codigos, unidades));
+  }
   @Test
   public void cp4() {
-    tienda.registrarLoteJuegos(new String[]{"OLD001"}, new int[]{195});
-    String[] codigos = {"NEW002"};
-    int[] unidades = {10};
-    assertEquals(-3, tienda.registrarLoteJuegos(codigos, unidades));
+    gestorJuegos.registrarLoteJuegos(new String[]{"OLD001"}, new int[]{195});
+    String[] codigos = {"ABC123", "DEF456","GHI789"};
+    int[] unidades = {7,6,5};
+    assertEquals(-3, gestorJuegos.registrarLoteJuegos(codigos, unidades));
+  }
+  @Test
+  public void cp5() {
+    String[] codigos = {"ABC123", "DEF456","GHI789"};
+    int[] unidades = {7,6,5};
+    assertEquals(18, gestorJuegos.registrarLoteJuegos(codigos, unidades));
   }
 }
