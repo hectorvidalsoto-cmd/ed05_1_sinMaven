fcdc553794eb2a803519d4344f2e3ab28a17ec64
hvidal <hectorvidalsoto@gmail.com>
2026-04-22 10:48:44 +0200

    cambios

--- src/test/java/org/ed05_1/GestorJuegosTest.java
+++ src/test/java/org/ed05_1/GestorJuegosTest.java
@@ -1,5 +1,17 @@
 package org.ed05_1;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class GestorJuegosTest {
 
+  GestorJuegos tienda = new GestorJuegos();
+  @Test
+  public void cp4() {
+    tienda.registrarLoteJuegos(new String[]{"OLD001"}, new int[]{195});
+    String[] codigos = {"NEW002"};
+    int[] unidades = {10};
+    assertEquals(-3, tienda.registrarLoteJuegos(codigos, unidades));
+  }
 }
